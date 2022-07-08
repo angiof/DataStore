87fd3d088342e5ea90a3d2682ae69c4caad47830
angelo ferretti <31105678+angiof@users.noreply.github.com>
2022-07-08 17:38:54 +0200

    fixed
    fix varie

--- PersisTence/src/main/java/com/example/persistence/DataMangerParam.kt
+++ PersisTence/src/main/java/com/example/persistence/DataMangerParam.kt
@@ -3,6 +3,7 @@ package com.example.persistence
 import android.content.Context
 
 class DataMangerParam {
+
     fun saveParam(c: Context, key: String?, value: Any?) {
         val preferences = c.getSharedPreferences("code", Context.MODE_PRIVATE)
         val editor = preferences.edit()
--- app/src/main/java/com/example/datastore/MainActivity.kt
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.datastore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.passycorefeatures.UtilsXX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
